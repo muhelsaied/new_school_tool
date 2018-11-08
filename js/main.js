b20b71418eb5d8294a46f729b7cfec280fa2156a
muhamed <muhahmed2099@gmail.com>
2018-11-08 17:05:01 +0200

    huge fix

--- js/main.js
+++ js/main.js
@@ -19,31 +19,47 @@ if(getMenu === false){
 }
 
 // change image btn
-let slide_1 = document.querySelector(".sub-1");
-let slide_2 = document.querySelector(".sub-2");
-let slide_3 = document.querySelector(".sub-3");
-let slide_4 = document.querySelector(".sub-4");
+let slide_1 = document.querySelector(".s-1");
+let slide_2 = document.querySelector(".s-2");
+let slide_3 = document.querySelector(".s-3");
+let slide_4 = document.querySelector(".s-4");
 let side_1_appear = function(){
    slide_1.style.opacity = "1";
    slide_2.style.opacity = "0";
    slide_3.style.opacity = "0";
    slide_4.style.opacity = "0";
+   slide_1.style.display = "flex";
+   slide_2.style.display = "none";
+   slide_3.style.display = "none";
+   slide_4.style.display = "none";
 }
 let side_2_appear = function(){
    slide_1.style.opacity = "0";
    slide_2.style.opacity = "1";
    slide_3.style.opacity = "0";
    slide_4.style.opacity = "0";
+   slide_1.style.display = "none";
+   slide_2.style.display = "flex";
+   slide_3.style.display = "none";
+   slide_4.style.display = "none";
 }
 let side_3_appear = function(){
    slide_1.style.opacity = "0";
    slide_2.style.opacity = "0";
    slide_3.style.opacity = "1";
    slide_4.style.opacity = "0";
+   slide_1.style.display = "none";
+   slide_2.style.display = "none";
+   slide_3.style.display = "flex";
+   slide_4.style.display = "none";
 }
 let side_4_appear = function(){
    slide_1.style.opacity = "0";
    slide_2.style.opacity = "0";
    slide_3.style.opacity = "0";
    slide_4.style.opacity = "1";
+   slide_1.style.display = "none";
+   slide_2.style.display = "none";
+   slide_3.style.display = "none";
+   slide_4.style.display = "flex";
 }

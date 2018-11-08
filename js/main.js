9dfea9b05b671dfe008bbe5cb49bfaf8a1871992
muhamed <muhahmed2099@gmail.com>
2018-11-08 21:59:13 +0200

    secnodary

--- js/main.js
+++ js/main.js
@@ -9,13 +9,9 @@ let container = document.querySelector(".container");
 let appearMenu = function(){
 if(getMenu === false){
 	sideMenu.style.top ="15%";
-	// sideMenu.style.marginLeft = "0";
-	topNav.style.position = "fixed";
 	getMenu = true;
 }else if(getMenu === true){
-	// sideMenu.style.marginLeft = "-100%";
 	sideMenu.style.top ="-200%";
-	topNav.style.position = "unset";
 	getMenu = false;
 }
 }
